4ef49b77985e42d9ea63f7e754ad0dd40d1f9646
AshfaqAhmed89 <ashfaq@aiondigital.com>
2023-05-01 05:58:39 +0500

    finance marketplace

--- src/app/features/borrower/finance-calculator/finance-calculator.component.ts
+++ src/app/features/borrower/finance-calculator/finance-calculator.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FinanceOptionComponent } from '../../common/finance-option/finance-option.component';
 
 @Component({
   selector: 'app-finance-calculator',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FinanceOptionComponent],
   templateUrl: './finance-calculator.component.html',
   styleUrls: ['./finance-calculator.component.scss']
 })
--- src/app/features/borrower/finance-marketplace/finance-marketplace.component.ts
+++ src/app/features/borrower/finance-marketplace/finance-marketplace.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FinanceBriefV2Component } from '../../common/finance-brief-v2/finance-brief-v2.component';
 
 @Component({
   selector: 'app-finance-marketplace',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FinanceBriefV2Component],
   templateUrl: './finance-marketplace.component.html',
   styleUrls: ['./finance-marketplace.component.scss']
 })
