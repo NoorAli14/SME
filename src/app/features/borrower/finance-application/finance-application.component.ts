f2538973e9135043dd73fee858352ef58ea6eab8
AshfaqAhmed89 <ashfaq@aiondigital.com>
2023-05-01 06:13:12 +0500

    finance application and calcultor

--- src/app/features/borrower/finance-application/finance-application.component.ts
+++ src/app/features/borrower/finance-application/finance-application.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FinanceOptionComponent } from '../../common/finance-option/finance-option.component';
+import { NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-finance-application',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FinanceOptionComponent, NgbNavModule],
   templateUrl: './finance-application.component.html',
   styleUrls: ['./finance-application.component.scss']
 })
