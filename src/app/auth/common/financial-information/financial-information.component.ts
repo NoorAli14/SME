37f6ee12634c09c1365f635efeb96f3932538d01
mehboob94 <mehboob@aiondigital.com>
2023-05-03 09:43:00 +0400

    changes

--- src/app/auth/common/financial-information/financial-information.component.ts
+++ src/app/auth/common/financial-information/financial-information.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormProgressListComponent } from 'src/app/features/common/form-progress-list/form-progress-list.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-financial-information',
   standalone: true,
-  imports: [CommonModule, FormProgressListComponent],
+  imports: [CommonModule, FormProgressListComponent, RouterModule],
   templateUrl: './financial-information.component.html',
   styleUrls: ['./financial-information.component.scss']
 })
--- src/app/auth/common/ownership/ownership.component.ts
+++ src/app/auth/common/ownership/ownership.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormProgressListComponent } from 'src/app/features/common/form-progress-list/form-progress-list.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-ownership',
   standalone: true,
-  imports: [CommonModule, FormProgressListComponent],
+  imports: [CommonModule, FormProgressListComponent, RouterModule],
   templateUrl: './ownership.component.html',
   styleUrls: ['./ownership.component.scss']
 })
